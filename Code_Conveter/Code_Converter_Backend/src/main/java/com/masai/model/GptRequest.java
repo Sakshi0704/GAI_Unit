4a6ce99108ec6a5c6f31ab02269525944831397a
Sakshi0704 <banichoudhary0704@gmail.com>
2023-08-05 11:04:15 +0530

    code_converter_backed

---
+++ Code_Conveter/Code_Converter_Backend/src/main/java/com/masai/model/GptRequest.java
@@ -0,0 +1,20 @@
+package com.masai.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class GptRequest {
+
+	   private String model;
+	    private List<Message> messages;
+
+	    public GptRequest(String model, String prompt) {
+	        this.model = model;
+	        this.messages = new ArrayList<>();
+	        this.messages.add(new Message("user",prompt));
+	    }
+	
+}
